9072375373a24a58399fb973030fcd1a11fcd0d4
renamrsh <irenamarsh@gmail.com>
2025-06-03 12:38:46 +0200

    Small changes

--- app/src/main/java/com/example/wizytauweterynarza/MainActivity.java
+++ app/src/main/java/com/example/wizytauweterynarza/MainActivity.java
@@ -41,8 +41,6 @@ public class MainActivity extends AppCompatActivity {
         button = findViewById(R.id.button);
         data = findViewById(R.id.data);
 
-        wybranyGatunek = "";
-
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
                 this,
                 R.array.gatunek_spinner_options_array,
@@ -56,20 +54,15 @@ public class MainActivity extends AppCompatActivity {
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
                 wybranyGatunek = adapterView.getItemAtPosition(i).toString();
             }
-
             @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
-
-            }
+            public void onNothingSelected(AdapterView<?> adapterView) {}
         });
-
 
         mySeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
                 ileMaLatProgress.setText(String.valueOf(i));
             }
-
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {}
 
@@ -79,15 +72,10 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
-
         timePicker = findViewById(R.id.timePicker);
         timePicker.setIs24HourView(true);
         timePicker.setHour(16);
         timePicker.setMinute(00);
-
-
-
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
